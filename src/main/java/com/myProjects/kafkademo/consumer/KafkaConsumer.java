f797ac9819c5ada8fc42fda79c896b86f315d97a
marcinSzym <marcin.szymainda@atos.net>
2023-12-12 16:57:58 +0100

    Change implementation to handle json

--- src/main/java/com/myProjects/kafkademo/consumer/KafkaConsumer.java
+++ src/main/java/com/myProjects/kafkademo/consumer/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.myProjects.kafkademo.consumer;
 
+import com.myProjects.kafkademo.payload.Student;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -8,8 +9,13 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class KafkaConsumer {
 
-    @KafkaListener(topics="firstTopic", groupId = "myGroup")
-    public void consumeMsg(String msg){
+    // @KafkaListener(topics="firstTopic", groupId = "myGroup")
+    public void consumeMsg(String msg) {
         log.info(String.format("Consuming the message from myFirst topic: %s", msg));
+    }
+
+    @KafkaListener(topics = "firstTopic", groupId = "myGroup")
+    public void consumeJsonMsg(Student student) {
+        log.info(String.format("Consuming the message from myFirst topic: %s", student.toString()));
     }
 }
